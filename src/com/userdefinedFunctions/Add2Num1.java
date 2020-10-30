75d0fb962f497964bc46f0f46033116a6f3c602e
SrinivasNarayan97 <snarayan2505@gmail.com>
2020-10-30 20:08:16 +0530

    version2

---
+++ src/com/userdefinedFunctions/Add2Num1.java
@@ -0,0 +1,29 @@
+package com.userdefinedFunctions;
+
+public class Add2Num1 {
+
+
+    public static void main(String[] args) {
+        Test1 t=new Test1(); // Has a relation
+        t.login();
+
+
+
+      //  Test1.login();
+        int a=10;
+        int b=20;
+
+        System.out.println("A");
+        int c=sum(a,b)+50;
+
+        System.out.println("Total sum="+c);
+        System.out.println("B");
+    }
+
+    public static int sum(int x, int y){
+       // int z= x +y ;
+        //System.out.println("sum="+z);
+        return x+y;
+
+    }
+}
